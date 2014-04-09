5a4c1234a75606c2b15da989524201c9b1066bc9
Davide Bertola <dade@dadeb.it>
2014-04-09 12:00:31 +0200

    better java api

--- DashPlayer.c
+++ DashPlayer.c
@@ -1,37 +1,69 @@
 #include "DashPlayer.h"
 #include "dash-player.c"
 
-/*
- * Class:     DashPlayer
- * Method:    dash_player_jni_initialize
- * Signature: ()V
- */
-
-DashPlayer *player = NULL;
-
-JNIEXPORT void JNICALL Java_DashPlayer_dash_1player_1jni_1initialize
-  (JNIEnv *env, jclass class)
-{
+int once = 0;
+void initialize_once () {
+  if (once) return;
+  once = 1;
   gst_init (NULL, NULL);
-  player = g_object_new(DASH_TYPE_PLAYER, NULL);
-  char *uri =
-    "http://www-itec.uni-klu.ac.at/ftp/datasets/mmsys13/redbull_4sec.mpd";
-
-  g_object_set (G_OBJECT (player), "uri", uri, NULL);
-  g_object_set (G_OBJECT (player), "bandwidth-usage", (gulong) 1.0, NULL);
-  g_object_set (G_OBJECT (player), "max-bitrate", (guint) 1024*1024, NULL);
-
 }
 
-/*
- * Class:     DashPlayer
- * Method:    dash_player_play
- * Signature: ()V
- */
-JNIEXPORT void JNICALL Java_DashPlayer_dash_1player_1play
+
+JNIEXPORT jlong JNICALL Java_DashPlayer_dash_1player_1jni_1initialize
   (JNIEnv *env, jclass class)
 {
+  initialize_once();
+  DashPlayer *player = g_object_new(DASH_TYPE_PLAYER, NULL);
+  //g_object_set (G_OBJECT (player), "bandwidth-usage", (gulong) 1.0, NULL);
+  //g_object_set (G_OBJECT (player), "max-bitrate", (guint) 1024*1024, NULL);
+  return (jlong) player;
+}
+
+
+JNIEXPORT void JNICALL Java_DashPlayer_dash_1player_1play
+  (JNIEnv *env, jclass class, jlong reference)
+{
+  DashPlayer *player = (DashPlayer *) reference;
   dash_player_play (player);
 }
 
 
+JNIEXPORT void JNICALL Java_DashPlayer_dash_1player_1set_1uri
+  (JNIEnv *env, jclass class, jlong reference, jstring juri)
+{
+  DashPlayer *player = (DashPlayer *) reference;
+  const char *uri = (*env)->GetStringUTFChars(env, juri, 0);  
+  g_object_set (G_OBJECT (player), "uri", uri, NULL);
+}
+
+
+JNIEXPORT void JNICALL Java_DashPlayer_dash_1player_1set_1bandwidth_1usage
+  (JNIEnv *env, jclass class, jlong reference, jfloat jbandwidth_usage)
+{
+  DashPlayer *player = (DashPlayer *) reference;
+  g_object_set (G_OBJECT (player), "bandwidth-usage",
+      (gulong) jbandwidth_usage, NULL);
+}
+
+
+JNIEXPORT void JNICALL Java_DashPlayer_dash_1player_1set_1max_1bitrate
+  (JNIEnv *env, jclass class, jlong reference, jint jmax_bitrate)
+{
+  DashPlayer *player = (DashPlayer *) reference;
+  g_object_set (G_OBJECT (player), "max-bitrate",
+      (guint) jmax_bitrate, NULL);
+}
+
+JNIEXPORT void JNICALL Java_DashPlayer_dash_1player_1stop
+  (JNIEnv *env, jclass class, jlong reference)
+{
+  DashPlayer *player = (DashPlayer *) reference;
+  dash_player_stop (player);
+}
+
+JNIEXPORT void JNICALL Java_DashPlayer_dash_1player_1unref
+  (JNIEnv *env, jclass class, jlong reference)
+{
+  DashPlayer *player = (DashPlayer *) reference;
+  g_object_unref (player);
+}
--- DashPlayer.java
+++ DashPlayer.java
@@ -2,19 +2,65 @@
 public class DashPlayer {
 
     static {
-        System.load("/Users/dade/work/gstdashplayer/.libs/libdashplayer.dylib");
-        
+      // this hack is done because .loadLibrary seems not to work correctly
+      // on macosx (mavericks at least)
+      String path = System.getProperty("java.library.path");
+      System.load(path + "/.libs/libdashplayer.dylib");
+    }
+    
+    public long reference;
+
+    public DashPlayer() {
+      this.reference = DashPlayer.dash_player_jni_initialize();
     }
 
-    public static native void dash_player_jni_initialize();
+    public void setUri(String uri) {
+      DashPlayer.dash_player_set_uri(this.reference, uri);
+    }
 
-    public static native void dash_player_play();
+    public void play() {
+      DashPlayer.dash_player_play(this.reference);
+    }
 
+    public void setBandwidthUsage (float bandwidthUsage) {
+      DashPlayer.dash_player_set_bandwidth_usage(this.reference, bandwidthUsage);
+    }
+
+    public void setMaxBitrate (int maxBitrate) {
+      DashPlayer.dash_player_set_max_bitrate(this.reference, maxBitrate);
+    }
+
+    public void stop () {
+      DashPlayer.dash_player_stop(this.reference);
+    }
+
+    /* c api */
+    public static native long dash_player_jni_initialize();
+
+    public static native void dash_player_play(long player);
+
+    public static native void dash_player_set_uri(long player, String uri);
+
+    public static native void dash_player_set_bandwidth_usage(long player, float bandwidth_usage);
+
+    public static native void dash_player_set_max_bitrate(long player, int bitrate);
+    
+    public static native void dash_player_stop(long player);
+
+    public static native void dash_player_unref(long player);
+
+    /* sample usage */
     public static void main(String[] argv) {
-      DashPlayer.dash_player_jni_initialize();
-      DashPlayer.dash_player_play();
-      while(true){
-        int i = 1;
-      }
+      DashPlayer player = new DashPlayer();
+      player.setUri("http://www-itec.uni-klu.ac.at/ftp/datasets/mmsys13/redbull_4sec.mpd");
+      player.setBandwidthUsage((float) 1.0);
+      player.setMaxBitrate(1024*1024);
+      player.play();
+      try { Thread.sleep(15000); } catch (InterruptedException e) {}
+      player.stop();
+      try { Thread.sleep(15000); } catch (InterruptedException e) {}
+    }
+
+    public void finalize () {
     }
 }
